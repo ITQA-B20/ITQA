a97671fbe663ac1bf1d947c920246a511de88a77
Raguraj <99555816+sragu2000@users.noreply.github.com>
2025-01-04 17:26:06 +0530

    api base url changed

--- cypress/support/data.js
+++ cypress/support/data.js
@@ -2,7 +2,7 @@
 export const uiBaseURL = "https://www.saucedemo.com/v1/index.html"
 export const uiAuth = { username: 'standard_user', password: 'secret_sauce' };
 // API Testing
-export const baseURL = "http://13.48.136.65:7081"
+export const baseURL = "http://51.20.188.150:7081"
 export const adminAuthHeader = { Authorization: 'Basic ' + btoa('admin:password') };
 export const userAuthHeader = { Authorization: 'Basic ' + btoa('user:password') };
 
